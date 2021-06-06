c0f9da521e81246e4c401984754be06d8560e708
Chubak <chubakbidpaa@gmail.com>
2021-06-07 01:27:40 +0430

    added two functions

--- excel_reader/main.go
+++ excel_reader/main.go
@@ -6,6 +6,15 @@ import (
 	"github.com/360EntSecGroup-Skylar/excelize/v2"
 )
 
+func find_item(slice []string, val string) (int, bool) {
+	for i, item := range slice {
+		if item == val {
+			return i, true
+		}
+	}
+	return -1, false
+}
+
 func unique(intSlice []string) []string {
 	keys := make(map[string]bool)
 	list := []string{}
@@ -18,16 +27,15 @@ func unique(intSlice []string) []string {
 	return list
 }
 
-func get_cities(sheet string) (x, y []string) {
-	f, err := excelize.OpenFile("files/City_of_Kelowna.xlsx")
+func get_cities(file, sheet string) (x, y []string) {
+	f, err := excelize.OpenFile(file)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	var load_loc []string
 	var unload_loc []string
-	rows, err := f.GetRows("GLELAN-TOLARM")
-	fmt.Println(rows[1][3], rows[1][4])
+	rows, err := f.GetRows(sheet)
 	for _, row := range rows {
 		load_loc = append(load_loc, row[3])
 		unload_loc = append(unload_loc, row[4])
@@ -37,10 +45,47 @@ func get_cities(sheet string) (x, y []string) {
 
 }
 
+func get_activites(file, sheet string) (x map[string][][]string) {
+	f, err := excelize.OpenFile(file)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	activites_map := make(map[string][][]string)
+
+	rows, err := f.GetRows(sheet)
+
+	for _, row := range rows[1:] {
+		activites_map[row[2]] = append(activites_map[row[2]], row)
+
+	}
+
+	return activites_map
+
+}
+
+func filter_activites(activites map[string][][]string, cities []string) (x map[string][][]string) {
+	activites_map_filtered := make(map[string][][]string)
+
+	for key, rows := range activites {
+		for _, row := range rows {
+			_, found := find_item(cities, row[4])
+
+			if found {
+				activites_map_filtered[key] = append(activites_map_filtered[key], row)
+			}
+		}
+	}
+
+	return activites_map_filtered
+}
+
 func main() {
+	loc_load, loc_unload := get_cities("files/City_of_Kelowna.xlsx", "GLELAN-TOLARM")
+	activities := get_activites("files/Shift_Detail_Report_2021-06-021.xlsx", "Data")
 
-	var load_loc, unload_loc = get_cities("GLELAN-TOLARM")
+	filtered_load := filter_activites(activities, loc_load)
+	filtered_unload := filter_activites(activities, loc_unload)
 
-	fmt.Println(load_loc)
-	fmt.Println(unload_loc)
+	fmt.Println(filtered_load, filtered_unload)
 }
